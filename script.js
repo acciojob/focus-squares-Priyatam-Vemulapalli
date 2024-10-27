0a0449d89f2d90a023f77f3a7b2f9e1d3924ca5a
Priyatam-Vemulapalli <priyatamvemulapalli@gmail.com>
2024-10-27 11:35:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll('.square');
+
+squares.forEach((square, index) => {
+  square.addEventListener('mouseenter', () => {
+    squares.forEach((otherSquare, otherIndex) => {
+      if (otherIndex !== index) {
+        otherSquare.style.backgroundColor = '#6F4E37'; 
+      }
+    });
+  });
+
+  square.addEventListener('mouseleave', () => {
+    squares.forEach((otherSquare, otherIndex) => {
+      if (otherIndex !== index) {
+        otherSquare.style.backgroundColor = '#E6E6FA'; 
+      }
+    });
+  });
+});
